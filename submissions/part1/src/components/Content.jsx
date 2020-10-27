9b2b6d347c36f04e0f9cd33de19bd645f5c91fd6
Amul Garg <amulg2012@gmail.com>
2020-10-27 22:41:29 +0530

    part 1 - completed exercise 1.3

--- submissions/part1/src/components/Content.jsx
+++ submissions/part1/src/components/Content.jsx
@@ -2,9 +2,9 @@ import Part from 'components/Part';
 
 const Content = (props) => {
     return <>
-        <Part part={props.part1} exercises={props.exercises1}/>
-        <Part part={props.part2} exercises={props.exercises2}/>
-        <Part part={props.part3} exercises={props.exercises3}/>
+        <Part part={props.part1.name} exercises={props.part1.exercises}/>
+        <Part part={props.part2.name} exercises={props.part2.exercises}/>
+        <Part part={props.part3.name} exercises={props.part3.exercises}/>
     </>;
 }
 
