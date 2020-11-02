b1c8678fe3924a85b1e68407991fdd8c63fa3970
Amul Garg <amulg2012@gmail.com>
2020-11-02 10:56:35 +0530

    part 2 - completed exercises 2.2 and 2.3

--- submissions/part2/courseinfo/src/components/Course.jsx
+++ submissions/part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from 'components/Header';
 import Content from 'components/Content';
+import Total from 'components/Total';
 
 const Course = ({course}) => {
     return <div>
         <Header course={course.name}/>
         <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
     </div>;
 }
 
