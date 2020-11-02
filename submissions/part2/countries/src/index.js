180eb50a824a0e908836781701305ca3737e6a59
Amul Garg <amulg2012@gmail.com>
2020-11-02 15:55:57 +0530

    part 2 - completed exercise 2.13, show country view on click

--- submissions/part2/countries/src/index.js
+++ submissions/part2/countries/src/index.js
@@ -2,10 +2,24 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+const CountryDetails = ({country}) => {
+  return <div>
+    <h2>{country.name}</h2>
+    <p>capital {country.capital}</p>
+    <p>population {country.population}</p>
+    <h3>languages</h3>
+    <ul>
+      {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
+    </ul>
+    <img src={country.flag} alt={`flag of ${country.name}`} height="100" width="100"/>
+  </div>;
+}
+
 const App = () => {
 
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+  const [countriesToView, setCountriesToView] = useState([]);
 
   useEffect(()=>{
     axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
@@ -18,6 +32,10 @@ const App = () => {
     setSearch(event.target.value);
   }
 
+  const showCountryView = (countryCode) => {
+    setCountriesToView(countriesToView.concat(countryCode));
+  }
+
   const renderFilteredCounties = () => {
     let filteredCountries = countries;
     if(search!=='')
@@ -27,19 +45,15 @@ const App = () => {
 
     if(filteredCountries.length === 1){
       const country = filteredCountries[0];
-      return <div>
-        <h2>{country.name}</h2>
-        <p>capital {country.capital}</p>
-        <p>population {country.population}</p>
-        <h3>languages</h3>
-        <ul>
-          {country.languages.map(language => <li key={language.iso639_2}>{language.name}</li>)}
-        </ul>
-        <img src={country.flag} alt={`flag of ${country.name}`} height="100" width="100"/>
-      </div>;
+      return <CountryDetails country={country}/>;
     }
 
-    return <div>{filteredCountries.map(country => <div key={country.alpha3Code}>{country.name}</div>)}</div>;
+    return <div>{filteredCountries.map(country => {
+      return <div key={country.alpha3Code}>
+        {country.name} <button onClick={() => showCountryView(country.alpha3Code)}>show</button>
+        {countriesToView.includes(country.alpha3Code) ? <CountryDetails country={country}/> : null}
+      </div>;
+    })}</div>;
   }
 
   return <div>
@@ -51,4 +65,4 @@ const App = () => {
 ReactDOM.render(
   <App/>,
   document.getElementById('root')
-);
+)
