ece803ad90c8a0ff2120ad709b107ad72d93a0d0
Amul Garg <amulg2012@gmail.com>
2020-11-02 11:02:42 +0530

    part 2 - completed exercises 2.4 and 2.5

--- submissions/part2/courseinfo/src/components/Total.jsx
+++ submissions/part2/courseinfo/src/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = (props) => {
     const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-    return <p>Number of exercises {totalExercises}</p>;
+    return <p><b>Total of {totalExercises} exercises</b></p>;
 }
 
 export default Total;
