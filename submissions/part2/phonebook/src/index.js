4340f0c9356812aa49c3625fdbbb513af7e3ea20
Amul Garg <amulg2012@gmail.com>
2020-11-02 14:27:52 +0530

    part 2 - completed exercise 2.11 (json server on port 3002)

--- submissions/part2/phonebook/src/index.js
+++ submissions/part2/phonebook/src/index.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const Filter = ({value, handleFilterChange}) => {
   return <div>filter shown with <input value={value} onChange={handleFilterChange}/></div>;
@@ -30,12 +31,7 @@ const Persons = ({persons, filter}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [ persons, setPersons ] = useState([]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ filter, setFilter ] = useState('');
@@ -66,6 +62,14 @@ const App = () => {
     }
 
   }
+
+  useEffect(()=>{
+    console.log('useeffect');
+    axios.get('http://localhost:3002/persons').then((response) => {
+      console.log('response', response);
+      setPersons(response.data);
+    });
+  }, []);
   
   return (
     <div>
